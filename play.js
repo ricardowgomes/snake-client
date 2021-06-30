5f886915e71a5d9190df58bb8c440bd5587b6ead
Ricardo W. Gomes <ricardowgomes@outlook.com>
2021-06-30 02:57:53 +0000

    added resources to connect on local host

---
+++ play.js
@@ -0,0 +1,22 @@
+// Main file
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost", // IP address here,
+    port: 50541// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Say hello to my little friend");
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();

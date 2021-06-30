78a58ac4c86098e2675b9187a684df6bb7f85081
Ricardo W. Gomes <ricardowgomes@outlook.com>
2021-06-30 15:55:00 +0000

    added input.js

---
+++ input.js
@@ -0,0 +1,19 @@
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+
+  stdin.on("data", handleUserInput);
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+module.exports = {
+  setupInput
+};
--- play.js
+++ play.js
@@ -1,20 +1,5 @@
 const { connect } = require("./client");
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-const setupInput = function () {
-  const stdin = process.stdin;
-
-  stdin.on("data", handleUserInput);
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();

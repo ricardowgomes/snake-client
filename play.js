aaa884c777a4155f3015b564e7106a0a3838691d
Ricardo W. Gomes <ricardowgomes@outlook.com>
2021-06-30 15:42:37 +0000

    added event listener

--- play.js
+++ play.js
@@ -1,4 +1,23 @@
 const { connect } = require("./client");
 
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+
+  stdin.on("data", handleUserInput);
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
 console.log("Connecting ...");
 connect();
+
+setupInput();
+
